a4a2c4bd3ed22c76d9c69afd063f70b2c37defb6
Katie Codd <42721661+Kcodd09@users.noreply.github.com>
2021-05-08 14:02:54 -0700

    working on cowtimer

--- src/_06_gridworld/gridworldRunner.java
+++ src/_06_gridworld/gridworldRunner.java
@@ -1,17 +1,38 @@
 package _06_gridworld;
 
+import java.awt.Color;
+import java.util.Random;
+
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class gridworldRunner {
 	public static void main(String[] args) {
-		World world = new World();
+		World<Actor> world = new World<Actor>();
+		Random ranX = new Random();
+		Random ranY = new Random();
+		Color color = new Color(255);
+		int x = ranX.nextInt(10);
+		int y = ranY.nextInt(10);
 		Bug bug = new Bug();
-		Location location = new Location(10, 5);
+		Bug bug2 = new Bug();
+		Flower flower = new Flower();
+		Location location = new Location(1, 5);
+		Location loc = new Location(0,0);
+		Location ranLoc = new Location(x, y);
 		world.show();
 		world.add(location, bug);
-		bug.getLocation();
+		world.add(ranLoc, bug2);
+		bug2.setColor(color);
+		bug.turn();
+		bug.turn();
+		world.add(loc, flower);
+		for (int i = 0; i < 97; i++) {
+			world.add(world.getRandomEmptyLocation(), flower);
+		}
 		
 	}
 	
